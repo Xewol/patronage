2e13ed05802ce08092aea1fcd89d7d830491ab11
Xewol <bartekdeluga0702@gmail.com>
2022-12-21 15:56:38 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,120 @@
+'use strict'
+
+//TODO load from storage
+const authroize = () => {
+  if (!localStorage.session) return undefined
+
+  return 'user'
+}
+
+//! USER DATA
+let currentUser = authroize()
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (!currentUser) {
+    offlineView()
+  }
+})
+
+const handleError = id => {
+  const input = document.querySelector(`#${id}`)
+  console.log(input)
+  input.classList.add('error')
+  const parent = input.parentElement
+  const error = document.createElement('p')
+  error.textContent = `Błąd`
+  error.classList.add('error-text')
+  parent.append(error)
+}
+
+const register = () => {
+  const inputs = document.querySelectorAll('input')
+  for (let input of inputs) {
+    input.classList.remove('error')
+  }
+  document.querySelectorAll('.error-text').forEach(el => el.remove())
+
+  const [username, password, email, rep_email] = inputs
+  if (!username.value) handleError(username.id)
+  if (!password.value) handleError(password.id)
+  if (!email.value) handleError(email.id)
+  if (!rep_email.value) handleError(rep_email.id)
+}
+
+const offlineView = () => {
+  const app = document.querySelector('#app')
+  app.innerHTML = content('notification')
+  //get login and register wrapper if user isn't logged in
+  const actionBtns = document.querySelector('.btn-wrapper')
+  //grab previous html state in case someone wants to return from form
+  const prevHtmlState = app.innerHTML
+
+  for (let button of actionBtns.childNodes) {
+    button.addEventListener('click', () => {
+      //remove class of both at the start
+      for (let btn of actionBtns.children) {
+        btn.classList.remove('hidden')
+      }
+
+      app.innerHTML = content(button.id)
+      //hide buttons when form is on
+      button.classList.add('hidden')
+
+      document.querySelector('#return').addEventListener('click', () => {
+        app.innerHTML = prevHtmlState
+        //unhide buttons when form is off
+        button.classList.remove('hidden')
+      })
+    })
+  }
+}
+
+//return html based on what user clicked
+const content = view => {
+  let html = ``
+
+  switch (view) {
+    case 'register':
+      html = `<form class="form-style" ><span class="action">Rejestracja</span>
+    <div>
+      <label for="username">Nazwa użytkownika</label>
+      <input type="text" id="username" />
+    </div>
+    <div>
+      <label for="password">Hasło</label>
+      <input type="password" id="password" />
+    </div>
+    <div>
+      <label for="mail">Email</label>
+      <input type="text" id="mail" />
+    </div>
+    <div>
+      <label for="rep_mail">Powtórz email</label>
+      <input type="text" id="rep_mail" />
+    </div>
+    <button class="btn submit" type="button" onclick="register()">Utwórz konto</button>
+      <button class="btn return" id="return" type="button">Powrót</button>
+    </form`
+      break
+    case 'login':
+      html = `<form class="form-style"><span class="action">Logowanie</span>
+      <div>
+        <label for="username">Nazwa użytkownika</label>
+        <input type="text" id="username" />
+      </div>
+      <div>
+        <label for="password">Hasło</label>
+        <input type="password" id="password" />
+      </div>
+
+      <button class="btn submit" type="button">Zaloguj</button>
+      <button class="btn return" id="return" type="button">Powrót</button>
+      </form`
+      break
+    case 'notification':
+      html = `<p class="notif">Zaloguj się, by sprawdzić swoje transakcje.</p>`
+      break
+  }
+
+  return html
+}
